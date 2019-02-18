377e258705ca6d6b1eb72ed41eb7479ab76990ce
ZhangLanDan <793015556@qq.com>
2019-02-18 18:24:42 +0800

    homework

--- Spider_with_Selenium/17track/17track.py
+++ Spider_with_Selenium/17track/17track.py
@@ -12,11 +12,95 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import xlwings as xw
 
 
-wb = xw.Book(filename)
+wb = xw.Book('a.xlsx')
 sht = wb.sheets[0]
-values = sht.range(range).options(transpose=True).value
+values = sht.range('a85:a167').options(transpose=True).value
 
-total = [i+'\n' for i in values if i is not None]
+total = ['3A5V483604539',
+'CNELA0000029008YQ',
+'CNELA0000028216YQ',
+'CNELA0000027701YQ',
+'3A5V483573912',
+'CNELA0000027892YQ',
+'CNELA0000024749YQ',
+'CNELA0000028175YQ',
+'CNELA0000027442YQ',
+'CNELA0000026391YQ',
+'CNELA0000024262YQ',
+'3A5V483657568',
+'CNELA0000028020YQ',
+'CNELA0000028707YQ',
+'CNELA0000028525YQ',
+'CNELA0000028416YQ',
+'CNELA0000027674YQ',
+'CNELA0000028052YQ',
+'CNELA0000028557YQ',
+'3A5V483621171',
+'CNELA0000028848YQ',
+'CNELA0000024230YQ',
+'CNELA0000028098YQ',
+'3A5V483573275',
+'3A5V483759616',
+'CNELA0000024644YQ',
+'3A5V483666380',
+'CNELA0000024826YQ',
+'3A5V483621973',
+'CNELA0000027583YQ',
+'3A5V483121027',
+'CNELA0000028343YQ',
+'CNELA0000024367YQ',
+'CNELA0000026341YQ',
+'CNELA0000001806YQ',
+'3A5V483759645',
+'CNELA0000022024YQ',
+'CNELA0000024935YQ',
+'3A5V483440949',
+'CNELA0000026137YQ',
+'CNELA0000027929YQ',
+'CNELA0000029212YQ',
+'CNELA0000028666YQ',
+'CNELA0000028593YQ',
+'CNELA0000028193YQ',
+'CNELA0000029208YQ',
+'3A5V483572806',
+'CNELA0000024767YQ',
+'3A5V483271309',
+'3A5V483572969',
+'CNELA0000024494YQ',
+'CNELA0000029244YQ',
+'CNELA0000029585YQ',
+'CNELA0000023002YQ',
+'3A5V483609153',
+'CNELA0000027642YQ',
+'3A5V483573189',
+'3A5V483609114',
+'3A5V483609208',
+'3A5V483594546',
+'3A5V483609258',
+'3A5V483609206',
+'CNELA0000024408YQ',
+'3A5V483715531',
+'CNELA0000024476YQ',
+'3A5V483573201',
+'CNELA0000026482YQ',
+'3A5V483609118',
+'3A5V483560669',
+'3A5V483609220',
+'3A5V483609201',
+'3A5V483609116',
+'3A5V483609142',
+'3A5V483573183',
+'3A5V483571709',
+'3A5V483572850',
+'CNELA0000027424YQ',
+'CNELA0000024599YQ',
+'CNELA0000026155YQ',
+'3A5V483573179',
+'3A5V483609127',
+'CNELA0000024703YQ',
+'CNELA0000026496YQ']
+
+total = [i +'\n' for i in total]
 
 
 def get_info():
@@ -63,13 +147,13 @@ def get_info():
         button.click()
         
         #跳过指引
-        if n ==1:
+        if n ==2:
             
             wait.until(EC.presence_of_element_located((By.XPATH,"/html/body/div[5]/div/div[5]/a[1]/i")))
             button = br.find_element_by_xpath("/html/body/div[5]/div/div[5]/a[1]/i")
             button.click()
         #复制结果
-        time.sleep(5)
+        time.sleep(8)
         br.find_element_by_xpath("//*[@id='jcPacakgeMenu']/div/button[1]").click()
         paste = pyperclip.paste()
         print(paste.replace('\n',''))
